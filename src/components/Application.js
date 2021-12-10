3013b1c4e32917eab1177e42b85c23c277bd55c6
jeff <jeffk713@gmail.com>
2021-12-09 23:01:39 -0800

    create GET request for appointmets in useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,54 +6,27 @@ import Appointment from './Appointment';
 
 import 'components/Application.scss';
 
-const appointments = [
-  {
-    id: 1,
-    time: '12pm',
-  },
-  {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 3,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
-  },
-  {
-    id: 3,
-    time: '2pm',
-  },
-  {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Archie Andrews',
-      interviewer: {
-        id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg',
-      },
-    },
-  },
-  {
-    id: 5,
-    time: '4pm',
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
   const [days, setDays] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/days').then(res => {
-      console.log(res.data);
-      setDays(res.data);
-    });
+    axios
+      .get('/api/days')
+      .then(res => {
+        console.log(res.data); // array
+        setDays(res.data);
+      })
+      .catch(err => console.log(err));
+
+    axios
+      .get('/api/appointments')
+      .then(res => {
+        console.log(res.data); //object
+        setAppointments(Object.values(res.data));
+      })
+      .catch(err => console.log(err));
   }, []);
 
   return (
